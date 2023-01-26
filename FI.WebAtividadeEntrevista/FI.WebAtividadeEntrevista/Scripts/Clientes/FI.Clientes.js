cde6f0ae32ee602de7a23464238a25e38b91e697
igor-goes <igor.nascimento@bandtec.com.br>
2023-01-26 15:34:56 -0300

    procjet 50%

---
+++ FI.WebAtividadeEntrevista/FI.WebAtividadeEntrevista/Scripts/Clientes/FI.Clientes.js
@@ -0,0 +1,115 @@
+﻿
+$(document).ready(function () {
+    $('#formCadastro').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: urlPost,
+            method: "POST",
+            data: {
+                "NOME": $(this).find("#Nome").val(),
+                "CEP": $(this).find("#CEP").val(),
+                "CPF": $(this).find("#CPF").val(),
+                "Email": $(this).find("#Email").val(),
+                "Sobrenome": $(this).find("#Sobrenome").val(),
+                "Nacionalidade": $(this).find("#Nacionalidade").val(),
+                "Estado": $(this).find("#Estado").val(),
+                "Cidade": $(this).find("#Cidade").val(),
+                "Logradouro": $(this).find("#Logradouro").val(),
+                "Telefone": $(this).find("#Telefone").val()
+            },
+            error:
+                function (r) {
+                    if (r.status == 400)
+                        ModalDialog("Ocorreu um erro", r.responseJSON);
+                    else if (r.status == 500)
+                        ModalDialog("Ocorreu um erro", "Ocorreu um erro interno no servidor.");
+                },
+            success:
+                function (r) {
+                    ModalDialog("Sucesso!", r)
+                    $("#formCadastro")[0].reset();
+                }
+        });
+    })
+
+})
+
+function ModalDialog(titulo, texto) {
+    var random = Math.random().toString().replace('.', '');
+    var texto = '<div id="' + random + '" class="modal fade">                                                               ' +
+        '        <div class="modal-dialog">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '                    <p>' + texto + '</p>                                                                           ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+    $('body').append(texto);
+    $('#' + random).modal('show');
+}
+
+
+
+function ModalDialogBeneficiarios(titulo) {
+    var random = Math.random().toString().replace('.', '');
+    var texto = '<div id="' + random + '" class="modal fade bd-example-modal-lg">                                                               ' +
+        '        <div class="modal-dialog modal-lg">                                                                                 ' +
+        '            <div class="modal-content">                                                                            ' +
+        '                <div class="modal-header">                                                                         ' +
+        '                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>         ' +
+        '                    <h4 class="modal-title">' + titulo + '</h4>                                                    ' +
+        '                </div>                                                                                             ' +
+        '                <div class="modal-body">                                                                           ' +
+        '<div class="form-inline" >                                                                           ' +
+
+        '<div class="form-group">                                                                           ' +
+        '                   <label for="CPF">CPF:</label>                                                                                  ' +
+        '                   <input required="required" type="text" class="form-control" id="CPF" name="CPF" placeholder="Ex.: 000.000.000-00" maxlength="14"> ' +
+        '</div> ' +
+
+        '<div class="form-group">                                                                           ' +
+        '                   <label for="Nome">Nome:</label>                                                                     ' +
+        '                   <input required="required" type="text" class="form-control" id="Nome" name="Nome" placeholder="Ex.: João da Silva" maxlength="50"> ' +
+
+        '</div> ' +
+
+
+        '<div class="form-group">                                                                           ' +
+        '                   <button type="submit" class="btn btn-sm btn-bf btn-success"> Incluir</button>                         ' +
+        '</div> ' +
+
+        '</div>                                                                                                             ' +
+        '<div class="form-inline">                                                                           ' +
+
+        ' <div class="form-group"> ' +
+        '                   <label for="CPF">CPF:</label>                                                                    ' +
+        '</div> ' +
+        ' <div class="form-group"> ' +
+        '                   <label for="Nome">Nome:</label>                                                                 ' +
+        '</div> ' +
+        '</div> ' +
+
+
+
+        '                </div>                                                                                             ' +
+        '                <div class="modal-footer">                                                                         ' +
+        '                    <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>             ' +
+        '                                                                                                                   ' +
+        '                </div>                                                                                             ' +
+        '            </div><!-- /.modal-content -->                                                                         ' +
+        '  </div><!-- /.modal-dialog -->                                                                                    ' +
+        '</div> <!-- /.modal -->                                                                                        ';
+
+    $('body').append(texto);
+    $('#' + random).modal('show');
+}
